39865352728062ff9cacd2e4b653e10d10009371
ashleylia25 <meldylia25@gmail.com>
2025-11-27 10:07:43 +0100

    first commit

---
+++ SimpleCalculator/Program.cs
@@ -0,0 +1,34 @@
+﻿Console.Clear();
+Console.WriteLine("Napiste prvni cislo, znamenko a druhe cislo:");
+String cislo1 = Console.ReadLine();
+String znamenko = Console.ReadLine();
+String cislo2 = Console.ReadLine();
+
+int prvniCislo = int.Parse(cislo1);
+int druheCislo = int.Parse(cislo2);
+
+if (druheCislo == 0 && znamenko == "/")
+{
+    Console.WriteLine("Nejde delit nulou.");
+}
+else if (znamenko == "+")
+{
+    Console.WriteLine(prvniCislo + druheCislo);
+}
+else if (znamenko == "-")
+{
+    Console.WriteLine(prvniCislo - druheCislo);
+}
+else if (znamenko == "*")
+{
+    Console.WriteLine(prvniCislo * druheCislo);
+}
+else if (znamenko == "/")
+{
+    Console.WriteLine(prvniCislo / druheCislo);
+}
+else
+{
+    Console.WriteLine("Nelze");
+}
+
